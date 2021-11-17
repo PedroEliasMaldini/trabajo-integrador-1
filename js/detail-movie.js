388349b2f5619470303217b36c767dd790999294
MartinaArnaudeS <marnaude@udesa.edu.ar>
2021-11-17 14:58:49 -0300

    pagina de detalles pelicula js (martu)

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -5,5 +5,36 @@ let idPP = new URLSearchParams(location.search);
 let peliculaPopular = idPP.get("id");
 console.log(peliculaPopular)
 
-let urlPP = 'https://api.themoviedb.org/3/movie/popular?api_key=5879ede367a1cc1dbb7ecaf35f419c29' + peliculaPopular;
+let urlPP = 'https://api.themoviedb.org/3/movie/' + peliculaPopular + '?api_key=5879ede367a1cc1dbb7ecaf35f419c29';
+const imgUrl = 'https://image.tmdb.org/t/p/w500/'
 
+let titulo = document.querySelector('h2');
+let rating = document.querySelector('.pRating');
+let estreno = document.querySelector('.pEstreno');
+let duracion = document.querySelector('.pDuracion');
+let genero = document.querySelector('.pGenero');
+let sinopsis = document.querySelector('.pSinopsis');
+let imgPeli = document.querySelector('.img-Slector');
+
+fetch(urlPP)
+    .then(function (respuesta) {
+        return respuesta.json();
+    })
+    .then(function (datos) {
+            console.log(datos);
+            titulo.innerText += " " + datos.title;
+            rating.innerText += " " + datos.vote_average;
+            estreno.innerText += " " + datos.release_date;
+            duracion.innerText += " " + datos.runtime;
+            for (let i = 0; i < datos.genres.length; i++) {
+                let generoS = datos.genres;
+                genero.innerHTML += `<a href="detail2-genres.html?id=${generoS[i].id}"> ${generoS[i].name} </a>`;
+            }
+            sinopsis.innerText += " " + datos.overview;
+            imgPeli.src = imgUrl + '1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg';
+
+
+        }
+
+    )
+    .catch()
