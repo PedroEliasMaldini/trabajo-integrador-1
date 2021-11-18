d660940ba3614fcf12eaeb195a7fd6a638010fc9
PedroEliasMaldini <pmaldini@udesa.edu.ar>
2021-11-18 10:28:14 -0300

    avanzo en el js de generos de pelis y series y modificacion en el footer

--- js/genero.js
+++ js/genero.js
@@ -1,23 +1,105 @@
-// capturo lo que voy a necesitar
-let urlDeGenerosPeliculas = 
+
+window.addEventListener('load', function () {
+// capturo lo que voy a necesitar para las pelis
+let urlDeGenerosPeliculas = `https://api.themoviedb.org/3/genre/movie/list?api_key=5879ede367a1cc1dbb7ecaf35f419c29`
+let LinksDeGeneros = document.querySelector(`.detalles-g`)
 
 
 
-//Hacemos el fetch usando la base generica
-
-
+// Peliculas 
  fetch(urlDeGenerosPeliculas)
 .then(function(response){
     return response.json()
 })
 .then(function(data){
-// aca donde pasa toda la magia
+    console.log(data)
 
-console.log(data)
+
+LinksDeGeneros.innerHTML = `
+
+<article class="d-generos">
+<h3 class="titulo-g"> <a href="${data.genres[0].id}">Acción</a> </h3>
+
+
+</article>
+<article class="d-generos">
+<h3 class="titulo-g"> <a href="${data.genres[1].id}">Aventura</a> </h3>
+
+
+</article>
+<article class="d-generos">
+<h3 class="titulo-g"> <a href="${data.genres[10].id}">Terror</a> </h3>
+
+
+</article>
+<article class="d-generos">
+<h3 class="titulo-g"> <a href="${data.genres[3].id}">Comedia</a> </h3>
+
+
+</article>
+<article class="d-generos">
+<h3 class="titulo-g"> <a href="${data.genres[13].id}">Romance</a></h3>
+
+
+</article>
+`
+
+   
+
+
+
 })
 .catch(function(error){
 console.log(`Algo que no está bien está mal ${error}`)
 })   
 
 
+})
+
+//capturo lo que voy a necesitar para las series
+let UrlDeGenerosSeries = `https://api.themoviedb.org/3/genre/tv/list?api_key=5879ede367a1cc1dbb7ecaf35f419c29`
+let LinkDeGeneros2 = document.querySelector(`.d-generos-series`)
+
+
+//series
+fetch(UrlDeGenerosSeries)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+    LinkDeGeneros2.innerHTML = `
+    <article class="d-generos" >
+                <h3 class="titulo-g"><a href="${data.genres[5].id}">Drama</a></h3>
+
+
+            </article>
+            <article class="d-generos">
+                <h3 class="titulo-g"><a href="${data.genres[8].id}">Misterio</a></h3>
+
+
+            </article>
+            <article class="d-generos">
+                <h3 class="titulo-g"> <a href="${data.genres[3].id}">Crimen</a></h3>
+
+
+            </article>
+            <article class="d-generos">
+                <h3 class="titulo-g"> <a href="${data.genres[11].id}">Ficción Histórica</a></h3>
+
+
+            </article>
+            <article class="d-generos">
+                <h3 class="titulo-g"> <a href="${data.genres[10].id}">Thriller</a></h3>
+
+            
+            </article>
+    `
+    
+    
+})
+.catch(function(error){
+    console.log(`Algo que no está bien está mal ${error}`)
+  
+}) // importante no borrar 
 
