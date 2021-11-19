677c3c616bd1bf17e5523a4c8743faeb0d5e543f
MarianoMendelsohn <89272874+MarianoMendelsohn@users.noreply.github.com>
2021-11-19 17:04:13 -0300

    Generos

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -7,6 +7,8 @@ window.addEventListener('load', function () {
     let urlDeGenerosPeliculas = `https://api.themoviedb.org/3/genre/${idGet}?api_key=5879ede367a1cc1dbb7ecaf35f419c29`; 
     let tituloGenero = document.querySelector(`.titulo-fav`)
     let listaDePelis = document.querySelector(`.ul-detail2`)
+    let urlGeneroP = `https://api.themoviedb.org/3/movie/${tipoGet}?api_key=5879ede367a1cc1dbb7ecaf35f419c29` 
+
 
 
 
@@ -25,7 +27,19 @@ window.addEventListener('load', function () {
         })
         .catch()
 
-   
+   if(tipoGet='movie'){
+fetch (urlGeneroP)
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch()
+}
+   else{
+
+   }
 
 
 
