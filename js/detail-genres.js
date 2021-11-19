9140f41eed062a6bedf054aab5bbcc70c04ab7a3
MarianoMendelsohn <89272874+MarianoMendelsohn@users.noreply.github.com>
2021-11-19 11:09:06 -0300

    Detalles Genero

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,10 +1,14 @@
 window.addEventListener ('load', function () {
-})
+    
+let idDetG = new URLSearchParams(location.search);
 
-// Deberia ir el URL del endpoint + el apiKey que pasó Martina
-let urlDeDetallesDeGenero = 
+let detalleGenero = idDetG.get("id");
+console.log(detalleGenero)
 
-fetch('urldeDetallesDeGenero')
+let urlDetGen = 'https://api.themoviedb.org/3/movie/' + detalleGenero + '?api_key=5879ede367a1cc1dbb7ecaf35f419c29';
+
+
+fetch('urlDetGen')
 
 .then(function(response) {
     return response.json();
@@ -20,10 +24,14 @@ fetch('urldeDetallesDeGenero')
 
 // Debería ir un bucle con la info de todos los generos.
 
-for(i=0; i<X; i++) {
-    console.log(generoX +i);
-};
+//for(i=0; i<X; i++) {
+  //  console.log(generoX +i);
+//};
 // en "X" debería ir la cantidad de generos que quiero traer (?)
+})
+
+
+
 
 
 
