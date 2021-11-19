71d55a166ab68912bff23ae2c3113a49570a3a6e
PedroEliasMaldini <pmaldini@udesa.edu.ar>
2021-11-19 11:54:43 -0300

    detalle de generos

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,4 +1,60 @@
 window.addEventListener ('load', function () {
+
+// peliculas 
+let esteId = new URLSearchParams(location.search);
+let idGet =esteId.get("id")
+
+let urlDeGenerosPeliculas ='https://api.themoviedb.org/3/genre/movie/list/' + idGet + '?api_key=5879ede367a1cc1dbb7ecaf35f419c29'
+
+
+fetch(urlDeGenerosPeliculas)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+
+
+})
+.catch(function(error){
+    console.log(`El Errror fue ` + error)
+})
+
+console.log(idGet)
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* window.addEventListener ('load', function () {
 })
 
 // Deberia ir el URL del endpoint + el apiKey que pasó Martina
@@ -26,4 +82,6 @@ for(i=0; i<X; i++) {
 // en "X" debería ir la cantidad de generos que quiero traer (?)
 
 
+console.log(location.search)
 
+*/
